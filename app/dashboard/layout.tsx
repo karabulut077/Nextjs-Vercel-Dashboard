0d2d63762779df01fcb5b14db65c0834c8b611b4
karabulut <mehmetcan.karabulut@outlook.com.tr>
2023-12-14 15:53:43 +0300

    implemented page and component based streaming

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -5,9 +5,11 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
   return (
     <>
-      <div className='w-100vw h-32 bg-lime-200'>
+      {/*
+        <div className='w-100vw h-32 bg-lime-200'>
 
-      </div>
+        </div>
+      */}
       <div className="md:flex-row flex h-screen flex-col md:overflow-hidden">
         <div className="w-full flex-none md:w-80">
           <SideNav />
